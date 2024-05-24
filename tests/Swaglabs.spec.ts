9ec1f5fb51d272ffca64e043007e7092fad97202
Raj Patel <rajpatel@hyperlinkinfosystem.net.in>
2024-05-24 17:03:59 +0530

    Update Test case for Githib check

--- tests/Swaglabs.spec.ts
+++ tests/Swaglabs.spec.ts
@@ -2,31 +2,60 @@ import { test, expect } from '@playwright/test';
 import { LoginPO } from './pageobject/LoginPO';
 import { HomePO } from './pageobject/HomePO';
 import { YourCartPO } from './pageobject/YourCartPO';
-test('Verify the Swag labs Functionality ', async ({ page }) => {
+test.describe('Verify the Swag labs Functionality ', () => {
 
-    const Login = new LoginPO(page)
-    const Home = new HomePO(page)
-    const YourCart = new YourCartPO(page)
+    test("Verify that user can add the Product into cart", async ({ page }) => {
+        const Login = new LoginPO(page)
+        const Home = new HomePO(page)
+        const YourCart = new YourCartPO(page)
 
-    // Step 1: Navigate to the swag labs website
-    await page.goto("/");
+        // Step 1: Navigate to the swag labs website
+        await page.goto("/");
 
-    // Step 2: Enter username, password, and click on the login button
-    await Login.enterUserNameAndPassword("standard_user", "secret_sauce")
-    await Login.clickOnLoginButton()
+        // Step 2: Enter username, password, and click on the login button
+        await Login.enterUserNameAndPassword("standard_user", "secret_sauce")
+        await Login.clickOnLoginButton()
 
-    // Step 3: Verify the Product page
-    expect(await Home.getTextOfHeaderText()).toEqual("Products")
+        // Step 3: Verify the Product page
+        expect(await Home.getTextOfHeaderText()).toEqual("Products")
 
-    // Step 4: Add Two product into the cart
-    await Home.clickOnTheAddToCartButton("Sauce Labs Backpack")
-    await Home.clickOnTheAddToCartButton("Sauce Labs Bike Light")
+        // Step 4: Add Two product into the cart
+        await Home.clickOnTheAddToCartButton("Sauce Labs Backpack")
+        await Home.clickOnTheAddToCartButton("Sauce Labs Bike Light")
 
-    // Step 5: Verify Cat badge has a valid count
-    expect(await Home.getTextOfBadge()).toEqual("2")
-    await Home.clickOnCartButton()
+        // Step 5: Verify Cat badge has a valid count
+        expect(await Home.getTextOfBadge()).toEqual("2")
+        await Home.clickOnCartButton()
 
-    // Step 6: Verify Your cart screen
-    expect(await YourCart.getHeaderTextOfYourCart()).toEqual("Your Cart")
+        // Step 6: Verify Your cart screen
+        expect(await YourCart.getHeaderTextOfYourCart()).toEqual("Your Cart")
+    })
+
+    test("Verify that user can add the Product into cart Part 2", async ({ page }) => {
+        const Login = new LoginPO(page)
+        const Home = new HomePO(page)
+        const YourCart = new YourCartPO(page)
+
+        // Step 1: Navigate to the swag labs website
+        await page.goto("/");
+
+        // Step 2: Enter username, password, and click on the login button
+        await Login.enterUserNameAndPassword("standard_user", "secret_sauce")
+        await Login.clickOnLoginButton()
+
+        // Step 3: Verify the Product page
+        expect(await Home.getTextOfHeaderText()).toEqual("Products")
+
+        // Step 4: Add Two product into the cart
+        await Home.clickOnTheAddToCartButton("Sauce Labs Backpack")
+        await Home.clickOnTheAddToCartButton("Sauce Labs Bike Light")
+
+        // Step 5: Verify Cat badge has a valid count
+        expect(await Home.getTextOfBadge()).toEqual("2")
+        await Home.clickOnCartButton()
+
+        // Step 6: Verify Your cart screen
+        expect(await YourCart.getHeaderTextOfYourCart()).toEqual("Your Cart")
+    })
 
 });
